53987bda735208826b8039de480c417354cbc0fb
Francis Tyers <ftyers@prompsit.com>
2020-12-18 14:26:32 +0000

    add script

--- scripts/check-encodings.sh
+++ scripts/check-encodings.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lt-expand apertium-ewo-fra.ewo-fra.dix | cut -f1 -d':' | grep '[áéíóúâêîôûàèìòùǎěǐǒǔāēīōū]'
+lt-expand apertium-ewo-fra.ewo-fra.dix | cut -f1 -d':' | grep --colour '[áéíóúâêîôûàèìòùǎěǐǒǔāēīōū]' 
